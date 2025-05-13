11fa42849a1ead9338f683ba865f8d3b1e7af1ca
ChiaraManeo <111811938+ChiaraManeo@users.noreply.github.com>
2025-05-13 20:31:47 -0300

    Update test_suites.py

--- test_suites.py
+++ test_suites.py
@@ -1,18 +1,6 @@
 import unittest
-
-class TestCalculadora(unittest.TestCase):
-    def test_somar(self):
-        self.assertEqual(2 + 3, 5)
-
-    def test_subtrair(self):
-        self.assertEqual(5 - 3, 2)
-
-class TestOutrasOperacoes(unittest.TestCase):
-    def test_multiplicar(self):
-        self.assertEqual(3 * 4, 12)
-
-    def test_dividir(self):
-        self.assertEqual(10 / 2, 5)
+from test_main import TestOperacoesMatematicas 
+from test_main import TestCalculadora
 
 suite = unittest.TestSuite()
 
